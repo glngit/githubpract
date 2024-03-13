f58204038a09acecbacabda88a8150b6e1aac42d
narasimha <youremail@example.com>
2024-03-13 15:55:40 +0530

    hari next 2

--- projects/ClassLibrary/Class1.cs
+++ projects/ClassLibrary/Class1.cs
@@ -8,3 +8,13 @@
         }
     }
 }
+namespace AdoIntroLibrary
+{
+    public class Class1
+    {
+        public void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
