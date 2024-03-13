7176e375c2323df4b6aa3202dfeb80b79468ec13
gln <gln@gmail.com>
2024-03-13 15:38:22 +0530

    diff gln1

--- pipelines/aws/main.tf
+++ pipelines/aws/main.tf
@@ -22,3 +22,29 @@ resource "aws_vpc" "vpc-test" {
   }
   ;lkasjdf;klajsdpf;lkjansdlfhjk
 }
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+    region     = "eu-west-1"
+}
+
+resource "aws_vpc" "vpc-test" {
+  cidr_block = "10.10.0.0/16"
+
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+  ;lkasjdf;klajsdpf;lkjansdlfhjk
+}
